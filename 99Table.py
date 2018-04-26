a4dd222b75dae41f935517a0cc6f552e0293461b
wzk563125509 <563125509@qq.com>
2018-04-26 11:06:51 +0800

    Delete 99Table.py

--- 99Table.py
+++
@@ -1,7 +0,0 @@
-for i in range(1,10):
-    print('')
-    for j in range(1,i+1):
-        #print("%d*%d=%d " %(i,j,i*j) , end = '')
-        a = '{0}*{1}={2}'.format(j,i,i*j)
-        print('{0:6} '.format(a),end='',sep='--')
-        #print('{0:1}'.format(),sep='',end='\n',filr=sys.stdout,flush=False)
